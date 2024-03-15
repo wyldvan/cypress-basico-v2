07377595cf54a563b8257d08350e795f33bea9c1
wyldvan <wyldvan.lopes@pigz.com.br>
2024-03-15 10:31:22 -0400

    fix: selectFile

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -123,11 +123,31 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .should('not.be.checked')
   })
 
-  it.only('seleciona um arquivo da pasta fixtures', function () {
+  it('seleciona um arquivo da pasta fixtures', function () {
     cy.get('input[type="file"]')
+      .should('not.have.value')
       .selectFile('cypress/fixtures/example.json')
-      .then(input => {
-        expect(input[0].files[0].name).to.equal('example.json')
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
       })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', function () {
+    cy.get('input[type="file"]')
+      .should('not.have.value')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function () {
+    cy.fixture("example.json").as('sampleFile')
+    cy.get('input[type="file"]')
+      .selectFile('@sampleFile')
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+
   })
 })
